5a89812973222ea5403ef71b3af3a221586d48c3
Darnell Champen <zerosalvation@gmail.com>
2022-06-28 15:15:46 -0400

    - set up mongoose, and middlewares.

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const express = require ("express");
-const app = express();
-const port = 3000
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 
-app.listen (port,() => {
-    console.log("listening on port" ,port)
-})
+const password = "ygrgwkVhhrTLy62";
+
+const app = express();
+const port = 3000;
+
+mongoose.connect(
+    process.env.MONGO_URI.replace("<password>", password),
+    { useNewUrlParser: true },
+    () => console.log("MongoDB Connection Established!", process.env.MONGO_URI)
+)
+db.on('error', err => console.log(err.message, ' MongoDB not running!'));
+db.on('disconnected', () => console.log('MongoDB disconnected!'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// ROUTES GO HERE.
+
+
+app.listen(port, () => {
+    console.log("listening on port", port)
+});
